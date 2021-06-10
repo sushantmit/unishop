f1274734fc4c6c32a4894b650a7bc1d8206d2a98
Sushant Prasad <prasadMac@Sushants-MacBook-Pro.local>
2021-06-10 19:00:29 +0530

    pagination complete

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,6 +4,7 @@ import { Table, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import Paginate from '../components/Paginate';
 import { listProducts, deleteProduct, createProduct } from '../actions/productActions';
 import { PRODUCT_CREATE_RESET } from '../constants/productConstants';
 
@@ -11,8 +12,10 @@ const ProductListScreen = ({ history, match }) => {
 
   const dispatch = useDispatch();
 
+  const pageNumber = match.params.pageNumber || 1;
+
   const productList = useSelector(state => state.productList);
-  const { loading, error, products } = productList;
+  const { loading, error, products, pages, page } = productList;
 
   const productDelete = useSelector(state => state.productDelete);
   const { loading:loadingDelete, error:errorDelete, success:successDelete } = productDelete;
@@ -31,9 +34,9 @@ const ProductListScreen = ({ history, match }) => {
     if(successCreate) {
       history.push(`/admin/product/${createdProduct._id}/edit`);
     } else {
-      dispatch(listProducts());
+      dispatch(listProducts('', pageNumber));
     }
-  }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct]);
+  }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct, pageNumber]);
 
   const deleteHandler = (id) => {
     if(window.confirm('Are you sure ?')) {
@@ -62,6 +65,7 @@ const ProductListScreen = ({ history, match }) => {
       { loadingCreate && <Loader /> }
       { errorCreate && <Message variant='danger'>{errorCreate}</Message> }
       {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
+        <>
         <Table striped hover responsive bordered className='table-sm'>
           <thead>
             <tr>
@@ -94,6 +98,8 @@ const ProductListScreen = ({ history, match }) => {
             ))}
           </tbody>
         </Table>
+        <Paginate pages={pages} page={page} isAdmin={true}></Paginate>
+        </>
       )}
     </>
   )
